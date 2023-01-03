fdf59f53d4ec78e0c09dbdf78b96e670577e9700
Frank Zheng <zhengger@gmail.com>
2023-01-03 16:24:16 +0800

    build: q001

---
+++ src/q001_0.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Print12AB() {
+	letter, number := make(chan bool), make(chan bool)
+	wait := sync.WaitGroup{}
+
+	go printNumber(number, letter, &wait)
+	go printLetter(number, letter, &wait)
+	number <- true
+}
+
+var printNumber = func(number, letter chan bool, wait *sync.WaitGroup) {
+	// wait.Add(1)
+	//
+	i := 1
+	for {
+		select {
+		case <-number:
+			fmt.Print(i)
+			i++
+			fmt.Print(i)
+			letter <- true
+		}
+	}
+}
+
+var printLetter = func(number, letter chan bool, wait *sync.WaitGroup) {
+	wait.Add(1)
+	l := 'A'
+	for {
+		select {
+		case <-letter:
+			if l >= 'Z' {
+				fmt.Print("Done")
+				wait.Done()
+			}
+			fmt.Print(string(l))
+			l++
+			fmt.Print(string(l))
+			number <- true
+		}
+	}
+}
+
+func main() {
+	Print12AB()
+	// time.Sleep(3 * time.Second)
+}
